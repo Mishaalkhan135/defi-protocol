e72de442fbee78f337fd64a7e266fa9f3e8e74fd
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-06-21 00:20:52 +0500

    added cards border color
    added cards border color

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const AboutCard = (props) => {
-  return (
-    <div className='flex flex-col border text-left rounded-2xl py-12 px-8'>
-    <div>
-      <div className='bg-[#00d8ff] inline-flex p-2 rounded-full'>
-        {props.icon}
-      </div>
-      <h3 className='text-xl font-bold py-4'>{props.heading}</h3>
-      <p>
-        {props.text}
-      </p>
-    </div>
-  </div>
-  )
-}
+	return (
+		<div className='flex flex-col text-left rounded-xl border-2 border-blue-500 py-12 px-8'>
+			<div>
+				<div className='bg-[#00d8ff] inline-flex p-2 rounded-full'>
+					{props.icon}
+				</div>
+				<h3 className='text-xl font-bold py-4'>{props.heading}</h3>
+				<p>{props.text}</p>
+			</div>
+		</div>
+	);
+};
 
-export default AboutCard
+export default AboutCard;
